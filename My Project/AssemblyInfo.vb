19f17ecec9f182111516afeabe5380dbe2e8c869
Sparta <spartaofomg@gmail.com>
2020-09-27 15:45:07 -0400

    200927 update with early track change

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2.0.20.0923")>
-<Assembly: AssemblyFileVersion("2.0.20.0923")>
+<Assembly: AssemblyVersion("2.0.20.0927")>
+<Assembly: AssemblyFileVersion("2.0.20.0927")>
