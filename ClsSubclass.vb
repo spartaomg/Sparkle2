8c66c702d4eb85dac746b88ca6ef0062c9b3f0be
Sparta <spartaofomg@gmail.com>
2021-09-21 12:24:20 -0400

    210921 - Creating blank HSFile if file doesn't exits, editor still needs to be updated...

--- ClsSubclass.vb
+++ ClsSubclass.vb
@@ -2,50 +2,50 @@
     'From https://www.codeproject.com/Articles/3234/Subclassing-in-NET-The-pure-NET-way
 
     Public Class SubClassing
-	Inherits System.Windows.Forms.NativeWindow
+        Inherits System.Windows.Forms.NativeWindow
 
-	Public Event CallBackProc(ByRef m As Message)
+        Public Event CallBackProc(ByRef m As Message)
 
-	Public Sub New(ByVal handle As IntPtr)
-	    On Error GoTo Err
+        Public Sub New(ByVal handle As IntPtr)
+            On Error GoTo Err
 
-	    AssignHandle(handle)
+            AssignHandle(handle)
 
-	    Exit Sub
+            Exit Sub
 Err:
-	    ErrCode = Err.Number
-	    MsgBox(ErrorToString(), vbOKOnly + vbExclamation, Reflection.MethodBase.GetCurrentMethod.Name + " Error")
+            ErrCode = Err.Number
+            MsgBox(ErrorToString(), vbOKOnly + vbExclamation, Reflection.MethodBase.GetCurrentMethod.Name + " Error")
 
-	End Sub
+        End Sub
 
-	Public Property SubClass() As Boolean = False
+        Public Property SubClass() As Boolean = False
 
-	Protected Overrides Sub WndProc(ByRef m As Message)
-	    On Error GoTo Err
+        Protected Overrides Sub WndProc(ByRef m As Message)
+            On Error GoTo Err
 
-	    If SubClass Then
-		RaiseEvent CallBackProc(m)
-	    End If
-	    MyBase.WndProc(m)
+            If SubClass Then
+                RaiseEvent CallBackProc(m)
+            End If
+            MyBase.WndProc(m)
 
-	    Exit Sub
+            Exit Sub
 Err:
-	    ErrCode = Err.Number
-	    MsgBox(ErrorToString(), vbOKOnly + vbExclamation, Reflection.MethodBase.GetCurrentMethod.Name + " Error")
+            ErrCode = Err.Number
+            MsgBox(ErrorToString(), vbOKOnly + vbExclamation, Reflection.MethodBase.GetCurrentMethod.Name + " Error")
 
-	End Sub
+        End Sub
 
-	Protected Overrides Sub Finalize()
-	    On Error GoTo Err
+        Protected Overrides Sub Finalize()
+            On Error GoTo Err
 
-	    MyBase.Finalize()
+            MyBase.Finalize()
 
-	    Exit Sub
+            Exit Sub
 Err:
-	    ErrCode = Err.Number
-	    MsgBox(ErrorToString(), vbOKOnly + vbExclamation, Reflection.MethodBase.GetCurrentMethod.Name + " Error")
+            ErrCode = Err.Number
+            MsgBox(ErrorToString(), vbOKOnly + vbExclamation, Reflection.MethodBase.GetCurrentMethod.Name + " Error")
 
-	End Sub
+        End Sub
 
     End Class
 
